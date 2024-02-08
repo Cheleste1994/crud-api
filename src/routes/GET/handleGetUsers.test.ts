a4a9571ebb84f8a67630930924f4e8efcc56a39e
Andrei <wolverines@tut.by>
2024-02-08 21:27:18 +0300

    feat: add tests handleGetUsers

---
+++ src/routes/GET/handleGetUsers.test.ts
@@ -0,0 +1,21 @@
+import { users } from '../../data/users';
+import { ServerResponse } from 'http';
+import { handleGetUsers } from './handleGetUsers';
+
+describe('handleGetUsers', () => {
+  test('should, return users', () => {
+    let result;
+
+    const resMock = {
+      statusCode: null,
+      end: (data: string) => {
+        result = data;
+      },
+      setHeader: jest.fn(),
+    } as unknown as ServerResponse;
+
+    handleGetUsers(resMock);
+
+    expect(JSON.parse(result || '')).toStrictEqual(users);
+  });
+});
